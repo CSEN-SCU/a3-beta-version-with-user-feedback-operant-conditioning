d6d915fc1bee988c010d443e12d9b1dd5aded347
jzlee@scu.edu <jzlee@scu.edu>
2023-05-17 15:57:25 -0700

    Deleting Unneeded Files and Worked on Popup UI

--- popup.js
+++ popup.js
@@ -1,7 +1,11 @@
-document.getElementById('start').addEventListener('click', function() {
-  chrome.runtime.sendMessage({command: 'start'});
+document.getElementById('start').addEventListener('click', function () {
+  chrome.runtime.sendMessage({ command: 'start' });
+  document.getElementById('start').style.visibility = 'hidden';
+  document.getElementById('stop').style.visibility = 'visible';
 });
 
-document.getElementById('stop').addEventListener('click', function() {
-  chrome.runtime.sendMessage({command: 'stop'});
+document.getElementById('stop').addEventListener('click', function () {
+  chrome.runtime.sendMessage({ command: 'stop' });
+  document.getElementById('stop').style.visibility = 'hidden';
+  document.getElementById('start').style.visibility = 'visible';
 });
