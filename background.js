5ef6089683f3554cc9288afc86b8b7ed57e7993c
jzlee@scu.edu <jzlee@scu.edu>
2023-05-18 14:41:04 -0700

    Working on Injecting DOM as notifications

--- background.js
+++ background.js
@@ -1,28 +1,27 @@
-console.log("background.js loaded");
+console.log('background.js loaded');
 
 function functionToInject() {
-    let audio = new Audio(chrome.runtime.getURL('sounds/death_sound.mp3'));
-    audio.play();
+  let audio = new Audio(chrome.runtime.getURL('sounds/death_sound.mp3'));
+  audio.play();
 }
 
-
 const socialMediaSites = [
-    'www.facebook.com', 
-    'www.twitter.com', 
-    'www.instagram.com', 
-    'www.linkedin.com',
-    'www.snapchat.com',
-    'www.pinterest.com',
-    'www.reddit.com',
-    'www.tiktok.com',
-    'www.whatsapp.com',
-    'www.youtube.com',
-    'www.netflix.com',
-    'www.twitch.tv',
-    'www.hulu.com',
-    'www.disneyplus.com',
-    'www.amazon.com/Prime-Video',
-    'www.spotify.com'
+  'www.facebook.com',
+  'www.twitter.com',
+  'www.instagram.com',
+  'www.linkedin.com',
+  'www.snapchat.com',
+  'www.pinterest.com',
+  'www.reddit.com',
+  'www.tiktok.com',
+  'www.whatsapp.com',
+  'www.youtube.com',
+  'www.netflix.com',
+  'www.twitch.tv',
+  'www.hulu.com',
+  'www.disneyplus.com',
+  'www.amazon.com/Prime-Video',
+  'www.spotify.com',
 ];
 
 const affirmations = [
@@ -34,21 +33,21 @@ const affirmations = [
   "That's the way, dear! Show those tasks who's boss!",
   "You're really knocking it out of the park! Keep going!",
   "Heavens to Betsy, you're doing fantastic!",
-  "Oh, I could just hug you! Keep up the great work!",
-  "You're making progress like a champ! Don't stop now!"
+  'Oh, I could just hug you! Keep up the great work!',
+  "You're making progress like a champ! Don't stop now!",
 ];
 
 const negativeMessages = [
-  "Get off that Facebook, young whippersnapper, and get back to work!",
+  'Get off that Facebook, young whippersnapper, and get back to work!',
   "Quit your Tweetin' and focus, or you'll feel my wrath!",
   "Oh, so you're on Instagram? How about Insta-GRANDMA? Now focus!",
-  "Snap out of your Snapchat and snap into productivity!",
+  'Snap out of your Snapchat and snap into productivity!',
   "TikTok, TikTok, time's a-wastin'! Stop scrolling and start working!",
   "You better LinkedIn with your task, or I'll come after you!",
-  "Pinterest-ing choice, but not when you have work to do!",
+  'Pinterest-ing choice, but not when you have work to do!',
   "I see you on YouTube, but I'd rather you be on You-Do-Your-Task!",
   "Reddit all about it: Grandma's not happy when you're not working!",
-  "Whatsapp with all this distraction? Get back to your task!"
+  'Whatsapp with all this distraction? Get back to your task!',
 ];
 
 let state = 'off';
@@ -59,13 +58,15 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.command === 'start') {
     state = 'on';
     console.log('Start button clicked');
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, { message: "Grandma is rooting for you. Let's start!" });
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {
+        message: "Grandma is rooting for you. Let's start!",
+      });
     });
   } else if (request.command === 'stop') {
     state = 'off';
     console.log('Stop button clicked');
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
       chrome.tabs.sendMessage(tabs[0].id, { message: "I'm so proud of you." });
     });
     if (timer) {
@@ -91,7 +92,8 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
       // Only send messages if the site has changed.
       if (currentSite !== site) {
         // Send a negative message immediately.
-        let message = negativeMessages[Math.floor(Math.random() * negativeMessages.length)];
+        let message =
+          negativeMessages[Math.floor(Math.random() * negativeMessages.length)];
         console.log('Sending negative message:', message);
         chrome.tabs.sendMessage(tabId, { message: message });
 
@@ -102,35 +104,46 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 
         // Set a timer to send another negative message after 10 seconds.
         setTimeout(() => {
-          message = negativeMessages[Math.floor(Math.random() * negativeMessages.length)];
+          message =
+            negativeMessages[
+              Math.floor(Math.random() * negativeMessages.length)
+            ];
           console.log('Sending negative message after 10 seconds:', message);
           chrome.tabs.sendMessage(tabId, { message: message });
         }, 10000); // 10 seconds
 
-          // Sending third negative message after 40 seconds
-          setTimeout(() => {
-            const message = negativeMessages[Math.floor(Math.random() * negativeMessages.length)];
-            console.log('Sending negative message:', message);
-            chrome.tabs.sendMessage(tabId, { message: message });
-          }, 40000);
+        // Sending third negative message after 40 seconds
+        setTimeout(() => {
+          const message =
+            negativeMessages[
+              Math.floor(Math.random() * negativeMessages.length)
+            ];
+          console.log('Sending negative message:', message);
+          chrome.tabs.sendMessage(tabId, { message: message });
+        }, 40000);
 
-          // Send final message after 60 seconds
-          setTimeout(() => {
-            chrome.tabs.sendMessage(tabId, { message: "Grandma has died...." });
-            console.log('Grandma died message sent');
-            
-            // Speak the final message
-            chrome.tts.speak('Grandma has died.');
-          }, 60000);
+        // Send final message after 60 seconds
+        setTimeout(() => {
+          chrome.tabs.sendMessage(tabId, { message: 'Grandma has died....' });
+          console.log('Grandma died message sent');
+
+          // Speak the final message
+          chrome.tts.speak('Grandma has died.');
+        }, 60000);
       }
     } else {
       // If the site is not a social media site and there's no timer currently set,
       // set a timer to send a positive message after 60 seconds.
       if (!timer) {
         timer = setTimeout(() => {
-          const message = affirmations[Math.floor(Math.random() * affirmations.length)];
+          const message =
+            affirmations[Math.floor(Math.random() * affirmations.length)];
           console.log('Sending positive message:', message);
           chrome.tabs.sendMessage(tabId, { message: message });
+          chrome.scripting.executeScript({
+            target: { tabId: tabId },
+            file: ['content.js'],
+          });
           timer = null;
         }, 60000);
       }
--- content.js
+++ content.js
@@ -1,20 +1,3 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.message) {
-      let notification = document.createElement('div');
-      notification.style.position = 'fixed';
-      notification.style.bottom = '10px';
-      notification.style.right = '10px';
-      notification.style.backgroundColor = 'white';
-      notification.style.border = '1px solid black';
-      notification.style.padding = '10px';
-      notification.style.zIndex = '10000';
-      notification.textContent = request.message;
-  
-      document.body.appendChild(notification);
-  
-      setTimeout(() => {
-        document.body.removeChild(notification);
-      }, 5000);
-    }
-  });
-  
+var div = document.createElement('div');
+document.body.appendChild(div);
+div.innerText = 'Test';
